74a1089eebdef720c194e0fef7ec0f4f38c933ff
erick <erickgabrielferreira80@gmial.com>
2024-09-19 15:28:41 -0300

    iniciando os exercicios de importar bibliotecas do python

--- Documents/import-Python/desafio_01.py
+++
@@ -1,1 +0,0 @@
-#Crie um programa que leia um número real e mostrem na tela a sua porção inteira. ex:6.127, porção inteira é 6.
--- Documents/import-Python/exemplo_02.py
+++
@@ -1,4 +0,0 @@
-from math import sqrt, floor
-numero = int(input('Digite um numero para saber a sua raiz quadrada: '))
-raiz = sqrt(numero)
-print(f'A raiz quadrada de {numero} é igua a {floor(raiz)}')
